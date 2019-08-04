5f1802e3dbbc51586d519d1f8c5cd267e73a05e5
meeae <meeaeking@gmail.com>
2019-08-04 14:54:51 -0500

    initial commit, create index action

---
+++ app/controllers/api/playdates_controller.rb
@@ -0,0 +1,10 @@
+class Api::PlaydatesController < ApplicationController
+  def index
+    location=params["location"]
+    term=params["term"]
+    categories=params["categories"]
+    @places = HTTP.get("https://api.yelp.com/v3/businesses/search?location=#{location}&term=#{term}&categories=#{categories}",headers:{Authorization: "Bearer #{ENV["API_KEY"]}"})
+    
+    render 'index.json.jb'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  namespace :api do
+  #   get "/photos" => "photos#index"
+    get "/playdates" => "playdates#index"
+  end
+end
